5e5206d4fd8ceda0620c3b49dd0b44cd586fa40f
shimpei.yano <shimpei.yano@ctc-g.co.jp>
2020-02-18 13:17:57 +0900

    検証のため入れてみる。

--- simple-loop/src/main/java/com/example/simple_loop/App.java
+++ simple-loop/src/main/java/com/example/simple_loop/App.java
@@ -1,5 +1,8 @@
 package com.example.simple_loop;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  *
@@ -7,6 +10,10 @@ package com.example.simple_loop;
 public class App {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("start infinite loop..");
+		SimpleServer server = new SimpleServer(8080);
+		
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		service.execute(server);
 		int count = 0;
 		while (true) {
 			Thread.sleep(2000);
