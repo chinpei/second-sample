e2431baf0b8c956fb700cd2cc624bf7c53e690e3
shimpei.yano <shimpei.yano@ctc-g.co.jp>
2020-02-26 13:56:39 +0900

    ActiveMQを使ってみた。

--- simple-loop/src/main/java/com/example/simple_loop/App.java
+++ simple-loop/src/main/java/com/example/simple_loop/App.java
@@ -3,6 +3,12 @@ package com.example.simple_loop;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.jms.JMSException;
 
 /**
  * Hello world!
@@ -12,7 +18,6 @@ public class App {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("start infinite loop..");
 //		SimpleServer server = new SimpleServer(8080);
-//		
 //		ExecutorService service = Executors.newSingleThreadExecutor();
 //		service.execute(server);
 		Locale locale = Locale.getDefault();
@@ -21,13 +26,46 @@ public class App {
 		System.out.println("========== [env start] ==========");
 		System.getenv().entrySet().forEach(System.out::println);
 		System.out.println("========== [env end] ==========");
-		
-		
-		int count = 0;
-		while (true) {
-			Thread.sleep(3000);
-			System.out.println(df.format(LocalDateTime.now()) + ",(" + (++count) + ")");
-		}
+
+//		int count = 0;
+//		while (true) {
+//			Thread.sleep(3000);
+//			System.out.println(df.format(LocalDateTime.now()) + ",(" + (++count) + ")");
+//		}
+		startReceiver();
+		startSender();
+	}
+
+	static void startSender() {
+		String tcpurl = System.getenv("AMQ_SAMPLE01_AMQ_TCP_PORT");
+		// queue01
+		// amquser01
+		// amqpass01
+		Sender sender = new Sender(tcpurl, "amquser01", "amqpass01", "queue01");
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+		scheduler.scheduleAtFixedRate(() -> {
+			try {
+				sender.send("testest");
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}, 1, 2, TimeUnit.SECONDS);
 
 	}
+
+	static void startReceiver() {
+		String tcpurl = System.getenv("AMQ_SAMPLE01_AMQ_TCP_PORT");
+		Receiver receiver = new Receiver(tcpurl, "amquser01", "amqpass01", "queue01");
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		service.execute(() -> {
+			try {
+				receiver.receive(text -> {
+					System.out.println("受信しました。->" + text);
+				});
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
 }
