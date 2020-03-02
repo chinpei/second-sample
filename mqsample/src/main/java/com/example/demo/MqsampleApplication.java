20f881ddd8950865e91f7e59a6181a2b46d0863c
shimpei.yano <shimpei.yano@ctc-g.co.jp>
2020-03-02 16:52:21 +0900

    xxx

--- mqsample/src/main/java/com/example/demo/MqsampleApplication.java
+++ mqsample/src/main/java/com/example/demo/MqsampleApplication.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.jms.JMSException;
 
@@ -17,20 +18,25 @@ public class MqsampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MqsampleApplication.class, args);
 		System.out.println("xxx");
-		startReceiver();
-		startSender();
+		String tcpurl = System.getenv("AMQ_SAMPLE01_AMQ_TCP_PORT");
+//		String tcpurl = "tcp://localhost:61616";
+		
+		
+		startReceiver(tcpurl);
+		startSender(tcpurl);
 	}
 
-	static void startSender() {
-		String tcpurl = System.getenv("AMQ_SAMPLE01_AMQ_TCP_PORT");
+	static void startSender(String tcpurl) {
+		
 		// queue01
 		// amquser01
 		// amqpass01
 		Sender sender = new Sender(tcpurl, "amquser01", "amqpass01", "queue01");
 		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+		AtomicInteger ai = new AtomicInteger(0);
 		scheduler.scheduleAtFixedRate(() -> {
 			try {
-				sender.send("testest");
+				sender.send("testest:" + ai.incrementAndGet());
 			} catch (JMSException e) {
 				e.printStackTrace();
 			}
@@ -38,8 +44,7 @@ public class MqsampleApplication {
 
 	}
 
-	static void startReceiver() {
-		String tcpurl = System.getenv("AMQ_SAMPLE01_AMQ_TCP_PORT");
+	static void startReceiver(String tcpurl) {
 		Receiver receiver = new Receiver(tcpurl, "amquser01", "amqpass01", "queue01");
 		ExecutorService service = Executors.newSingleThreadExecutor();
 		service.execute(() -> {
